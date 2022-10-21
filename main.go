7a3426a10a6b1ecad17976fecd6eda20eeecc4f9
akhil <akhilsails@gmail.com>
2022-10-21 11:04:19 +0530

    working stage-2 code

--- main.go
+++ main.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
+)
+
+type (
+	Customer struct {
+		Name         string    `json:"cName"`
+		Tel          uint64    `json:"cTel"`
+		Address      string    `json:"cAddress"`
+		ID           uint64    `json:"cID"`
+		RegisterDate time.Time `json:"cRegisterDate"`
+	}
+
+	CustomerResponse struct {
+		Name         string `json:"cName"`
+		Tel          uint64 `json:"cTel"`
+		Address      string `json:"cAddress"`
+		ID           uint64 `json:"cID"`
+		RegisterDate string `json:"cRegisterDate"`
+		Message      string `json:"msg"`
+	}
+
+	AllCustomers struct {
+		Size      uint64              `json:"size"`
+		Customers []*CustomerResponse `json:"customers"`
+		Message   string              `json:"msg"`
+	}
+
+	JustMSG struct {
+		Message string `json:"msg"`
+	}
+
+	ReportResponse struct {
+		TotalCustomers uint64 `json:"totalCustomers"`
+		Period         int    `json:"period"`
+		Message        string `json:"msg"`
+	}
+)
+
+var (
+	db          = map[uint64]*Customer{}
+	last uint64 = 0
 )
 
 func main() {
